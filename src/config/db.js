6fa89bd75467ab4ed71366ee15875d413eab94a0
Faraz Ayaz <farazayazkhan1.email@example.com>
2026-08-25 21:45:44 +0500

    updated db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client.js";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import dotenv from "dotenv";
 
-// .env file se URL read karne ke liye
-dotenv.config(); 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-// 1. Connection Pool banayen
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-// 2. Adapter ko initialize karein
 const adapter = new PrismaPg(pool);
 
-// 3. Prisma ko adapter ke sath start karein
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({
+  adapter,
+});
