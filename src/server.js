4e9eeff50c5ec4f199dff66d1c6c7644f07681de
Faraz Ayaz <farazayazkhan1.email@example.com>
2026-08-25 21:34:09 +0500

    updated server js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import productRoutes from "../src/routes/productRoutes.js";
+
+import productRoutes from "./routes/productRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
@@ -10,18 +11,21 @@ import orderRoutes from "./routes/orderRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://amazon-frontend-psi.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
-// ==========================================
-// API ROUTES
-// ==========================================
+// API routes
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
@@ -31,6 +35,4 @@ app.get("/", (req, res) => {
   res.send("Amazon Clone API is running perfectly! 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running smoothly on http://localhost:${PORT}`);
-});
+export default app;
